9bb61ec5f741ee1a0f310835e481fe57b2964343
Hasmukh Rathod <guru__@hotmail.com>
2019-12-11 14:32:17 +0100

    init

---
+++ myJavaScript.js
@@ -0,0 +1,91 @@
+"use strict";
+
+var Tiles = {
+  Wall: "tile-wall",
+  Space: "tile-space",
+  Goal: "tile-goal"
+};
+
+/*   Enum of CSS Classes for the moving elements   */
+var Entities = {
+  Character: "entity-player",
+  Block: "entity-block",
+  BlockDone: "entity-block-goal"
+};
+
+var tileMap01 = {
+  width: 19,
+  height: 16,
+  mapGrid: [
+    [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], ['W'], ['B'], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], ['W'], ['W'], ['W'], [' '], [' '], ['B'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], ['W'], [' '], [' '], ['B'], [' '], ['B'], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [['W'], ['W'], ['W'], [' '], ['W'], [' '], ['W'], ['W'], [' '], ['W'], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W']],
+    [['W'], [' '], [' '], [' '], ['W'], [' '], ['W'], ['W'], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], ['G'], ['G'], ['W']],
+    [['W'], [' '], ['B'], [' '], [' '], ['B'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], ['G'], ['G'], ['W']],
+    [['W'], ['W'], ['W'], ['W'], ['W'], [' '], ['W'], ['W'], ['W'], [' '], ['W'], ['P'], ['W'], ['W'], [' '], [' '], ['G'], ['G'], ['W']],
+    [[' '], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W']],
+    [[' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+    [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']]
+  ]
+};
+
+var myDiv = document.getElementById("myDiv");
+
+myDiv.innerHTML = "";
+
+var myBody = document.getElementsByTagName("body")[0];
+
+myBody.style.backgroundColor = "lightgray";
+
+console.log(myDiv);
+
+myDiv.style.width = (tileMap01.width * 50) + "px";
+myDiv.style.height = (tileMap01.height * 50) + "px";
+
+var player_Y = -1;
+var player_X = -1;
+
+for (let y = 0; y < tileMap01.height; y++) {
+  for (let x = 0; x < tileMap01.width; x++) {
+    const element = document.createElement("div");
+    element.classList.add("box");
+
+    console.log(tileMap01.mapGrid[y][x]);
+
+    if (tileMap01.mapGrid[y][x] == " ") {
+      element.classList.add("space");
+
+    } else if (tileMap01.mapGrid[y][x] == "W") {
+      element.classList.add("wall");
+
+    } else if (tileMap01.mapGrid[y][x] == "G") {
+      element.classList.add("goal");
+
+    } else if (tileMap01.mapGrid[y][x] == "B") {
+      element.classList.add("create");
+
+    } else if (tileMap01.mapGrid[y][x] == "P") {
+      element.classList.add("player");
+      player_Y = y;
+      player_X = x;
+    }
+
+    element.id = "x" + x + "y" + y;
+
+    myDiv.appendChild(element);
+  }
+}
+
+function playerMove(y_offset, x_offset) {
+
+
+}
+
+
